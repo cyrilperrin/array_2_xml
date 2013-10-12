615ac0a1e01a473baeb94c0f95ef20628af6fa27
cyrilperrin <perrin.cyril.88@gmail.com>
2013-10-13 00:39:21 +0200

    Improve Zend coding standards compliance

--- example/index.php
+++ example/index.php
@@ -1,18 +1,21 @@
 <?php
 
 // Require
-require('../src/array_2_xml.php');
+require ('../src/array_2_xml.php');
 
 // HTTP header
 header("content-type: text/xml");
 
 // Display XML
-echo array_2_xml('person',array(
-	'@firstname' => 'James',
-	'@lastname' => 'Bond',
-	'cars' => array(
-		array('car', '@model' => 'Vanquish', '@brand' => 'Aston Martin'),
-		array('car', '@model' => 'Esprit', '@brand' => 'Lotus'),
-		array('car', '@model' => 'Fastback', '@brand' => 'Mustang')
-	)
-));
+echo array_2_xml(
+    'person',
+    array(
+        '@firstname' => 'James',
+        '@lastname' => 'Bond',
+        'cars' => array(
+            array('car', '@model' => 'Vanquish', '@brand' => 'Aston Martin'),
+            array('car', '@model' => 'Esprit', '@brand' => 'Lotus'),
+            array('car', '@model' => 'Fastback', '@brand' => 'Mustang')
+        )
+    )
+);
--- src/array_2_xml.php
+++ src/array_2_xml.php
@@ -7,8 +7,9 @@
  * @param $root string root node name
  * @param $array array array
  * @return XML
- */  
-function array_2_xml($root, $array) {  
+ */
+function array_2_xml($root, $array)
+{ 
     // Create document
     $document = new DOMDocument('1.0', 'utf-8');  
       
@@ -17,38 +18,56 @@ function array_2_xml($root, $array) {
     $document->appendChild($root);
     
     // Create node function
-    $createNode = function(DOMDocument $document, $key, $value) use (&$createNode) {
-	    // Create node
-	    $node = $document->createElement($key,is_array($value) ? null : $value);  
-	    
-	    // Add sub-nodes
-	    if (is_array($value)) {  
-	        foreach ($value as $key_2 => $value_2) {  
-	            if(is_int($key_2)) {
-		    		// Create and add node
-		    		$node->appendChild($createNode($document,array_shift($value_2),$value_2));
-		    	} else if (substr($key_2,0,1) == '@') {  
-	                // Add attribute
-	                $node->setAttribute(substr($key_2,1),$value_2);  
-	            } else {  
-	                // Create and add sub-node
-	                $node->appendChild($createNode($document,$key_2,$value_2));  
-	            }  
-	        }  
-	    }  
-	      
-	    // Return node
-	    return $node;  
-	};
+    $createNode = function(DOMDocument $document, $key, $value)
+                  use (&$createNode)
+    {
+        // Subnodes ?
+        if (is_array($value)) {
+            // Create node
+            $node = $document->createElement($key);
+            
+            // Add subnodes
+            foreach ($value as $subKey => $subValue) {
+                if (is_int($subKey)) {
+                    // Create subnode
+                    $subNode = $createNode(
+                        $document,
+                        array_shift($subValue),
+                        $subValue
+                    );
+                    
+                    // Add subnode
+                    $node->appendChild($subNode);
+                } else if (substr($subKey, 0, 1) == '@') {  
+                    // Add attribute
+                    $node->setAttribute(substr($subKey, 1), $subValue);
+                } else {  
+                    // Create and add subnode
+                    $node->appendChild(
+                        $createNode($document, $subKey, $subValue)
+                    );
+                }  
+            } 
+            
+            // Return node
+            return $node;
+        } else {
+            // Create and return node
+            return $document->createElement($key, $value);
+        }
+    };
       
     // Fill document
     foreach ($array as $key => $value) {
-    	if(is_int($key)) {
-    		// Create and add node
-    		$root->appendChild($createNode($document,array_shift($value),$value));
-    	} else if (substr($key,0,1) == '@') {  
+        if (is_int($key)) {
+            // Create subnode
+            $subNode = $createNode($document,array_shift($value),$value);
+            
+            // Add subnode
+            $root->appendChild($subNode);
+        } else if (substr($key, 0, 1) == '@') {
             // Add attribute
-            $root->setAttribute(substr($key,1),$value);  
+            $root->setAttribute(substr($key, 1), $value);
         } else {  
             // Create and add node
             $root->appendChild($createNode($document,$key,$value));  
